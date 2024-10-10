ae5b09100461017e3b689eb36ab69d38b2a99673
Yubi <jacobkoberstein@gmail.com>
2024-10-10 17:04:04 +0200

    Fix route for logo

--- src/main/resources/static/js/renderNavbar.js
+++ src/main/resources/static/js/renderNavbar.js
@@ -14,7 +14,7 @@ export default function renderNavBar() {
   document.querySelector('header').innerHTML = `
     <nav class="navbar navbar-expand-lg" data-bs-theme="dark">
       <div class="container-fluid">
-        <a class="navbar-brand px-2 px-md-4" href="#">Classic Artists</a>
+        <a class="navbar-brand px-2 px-md-4" href="/">Classic Artists</a>
         <button
             class="navbar-toggler" type="button" data-bs-toggle="collapse"
             data-bs-target="#navbarNav" aria-controls="navbarNav"
